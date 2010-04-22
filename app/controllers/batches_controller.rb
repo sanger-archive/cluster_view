d5a86526a1f5c1d696eed773582a0d8c042f3200
Sean Dunn <sd9@sanger.ac.uk>
2010-04-22 12:54:47 +0100

    Refactored show method to remove unnecessary begin end block.

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -1,12 +1,10 @@
 class BatchesController < ApplicationController
 
   def show
-    begin
-      @batch = Batch.find(params[:id])
-    rescue(ActiveResource::ResourceNotFound)
-      @batch_number = params[:id]
-      render :batch_not_found
-    end    
+    @batch = Batch.find(params[:id])
+  rescue(ActiveResource::ResourceNotFound)
+    @batch_number = params[:id]
+    render :batch_not_found
   end
   
 end
