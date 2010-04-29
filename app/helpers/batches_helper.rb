92d798b1e0c4a7882b908b06af43e51f7ef8f715
Matthew Denner <md12@sanger.ac.uk>
2010-04-29 17:04:54 +0100

    Clicking the thumbnails feels more natural for bringing up original image

--- app/helpers/batches_helper.rb
+++ app/helpers/batches_helper.rb
@@ -13,8 +13,8 @@ module BatchesHelper
     )
   end
 
-  def link_to_full_size_image(image)
-    link_to(h(image.data_file_name), batch_image_path(:id => image.batch_id, :image_id => image.id))
+  def link_to_full_size_image(image, &block)
+    link_to(batch_image_path(:id => image.batch_id, :image_id => image.id), &block)
   end
 
   def status_of(batch)
