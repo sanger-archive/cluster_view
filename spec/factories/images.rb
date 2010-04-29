94842bf6cdff1b6a5f2dfa3832e98eb6470e7898
Sean Dunn <sd9@sanger.ac.uk>
2010-04-29 14:53:34 +0100

    Altered batches_controller to return the original image when using the #images action. Added a test to ensure that batches_controller#images returns the original image file rather than the thumbnail. Added a content type for the original image to the images factory.

--- spec/factories/images.rb
+++ spec/factories/images.rb
@@ -1,6 +1,7 @@
 Factory.define('Images for batch', :class => Image) do |image|
   image.sequence(:data_file_name) { |n| "%03i" % n }
   image.sequence(:position) { |n| n }
-  image.data_file "IMAGE"
-  image.data_thumbnail_file "THUMBNAIL"
+  image.data_file "ORIGINAL IMAGE FILE"
+  image.data_content_type "ORINGINAL IMAGE TYPE"
+  image.data_thumbnail_file "THUMBNAIL IMAGE FILE"
 end
