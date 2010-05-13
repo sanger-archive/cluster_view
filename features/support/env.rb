759a902ed43729911099f369e9c4a28f6db66d2b
Matthew Denner <md12@sanger.ac.uk>
2010-05-13 16:45:17 +0100

    [fixes 3487827] script/autospec is now working properly
    Turns out the transactional fixtures stuff had been commented out.

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,7 +9,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
 
 require 'cucumber/formatter/unicode' # Remove this line if you don't want Cucumber Unicode support
 require 'cucumber/rails/world'
-#require 'cucumber/rails/active_record'
+require 'cucumber/rails/active_record'
 #require 'cucumber/web/tableish'
 
 require 'active_resource/http_mock'
