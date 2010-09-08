75300ec554e317e6652471c860466993759d1aeb
Conny Brunnkvist <cb16@sanger.ac.uk>
2010-09-08 16:05:01 +0100

    Switch to PsdLogger

--- config/environment.rb
+++ config/environment.rb
@@ -55,4 +55,10 @@ Rails::Initializer.run do |config|
     File.directory?(lib = "#{dir}/lib") ? lib : dir
   end
   
+  unless ENV['RAILS_ENV'] == 'development' #Rails.env.development?
+    require 'psd_logger'
+    config.logger = PsdLogger.new
+    config.active_record.colorize_logging = false
+    config.log_level = :warn
+  end
 end
