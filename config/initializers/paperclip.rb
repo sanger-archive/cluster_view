13d6ca9495abbcac01d1ac9d64c43f1d56d5e045
Conny Brunnkvist <cb16@sanger.ac.uk>
2011-01-20 13:55:19 +0000

    We might need to avoid convert whining about TIFFs

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,4 @@
 # Do not swallow the errors produced by ImageMagick convert
 Paperclip.options[:swallow_stderr] = false 
+# Do not panic because a TIFF might have unregistered property fields
+Paperclip.options[:whiny] = false 
