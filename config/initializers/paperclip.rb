1371a21b1ff033f469ae6193b05449cd92009a56
Conny Brunnkvist <cb16@sanger.ac.uk>
2011-01-20 17:52:43 +0000

    Revert "We might need to avoid convert whining about TIFFs"
    This reverts commit 13d6ca9495abbcac01d1ac9d64c43f1d56d5e045.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,2 @@
 # Do not swallow the errors produced by ImageMagick convert
 Paperclip.options[:swallow_stderr] = false 
-# Do not panic because a TIFF might have unregistered property fields
-Paperclip.options[:whiny] = false 
