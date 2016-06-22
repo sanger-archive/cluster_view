a33453f86236b0ea43ba42dee86d7b4225fc6871
James Glover <james.glover@sanger.ac.uk>
2016-06-22 09:56:08 +0100

    Update some deprecation warnings
    - We still have deprecation warning comming from haml, but we'll
    wait for the rails 4 update before resolving that one.

--- config/environment.rb
+++ config/environment.rb
@@ -59,8 +59,8 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
-  
-  config.load_paths += Dir["#{RAILS_ROOT}/vendor/gems/**"].map do |dir| 
+
+  config.autoload_paths += Dir["#{RAILS_ROOT}/vendor/gems/**"].map do |dir|
     File.directory?(lib = "#{dir}/lib") ? lib : dir
   end
 end
